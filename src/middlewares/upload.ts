7fe165a586c036174ac5322eb6db6ae3e5230141
Eva <evagr4121@gmail.com>
2026-04-08 13:32:11 +0200

    Haciendo cambios en el .env y en el upload para usar el servicio de cloudinary

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,21 +1,25 @@
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
 
-// 1. Asegurarnos de que la carpeta existe para que no pete
-const dir = './uploads/recetas';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir, { recursive: true });
-}
+dotenv.config();
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, dir); // Los archivos irán aquí
-    },
-    filename: (req, file, cb) => {
-        // Nombre único: timestamp + extensión original
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+// 1. Configuramos Cloudinary con tus llaves del .env
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-export const upload = multer({ storage });
+// 2. Le decimos a Multer que use Cloudinary en lugar de tu carpeta local
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'refrimancia_uploads', // Se creará esta carpeta en tu nube
+        allowed_formats: ['jpg', 'png', 'jpeg', 'webp'], // Formatos seguros
+    } as any
+});
+
+// 3. Exportamos para usar en las rutas
+export const upload = multer({ storage: storage });
